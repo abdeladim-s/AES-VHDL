96b2d4801d29c1a08a92b2ef304eb5739e134a01
hp-pc <sadiki.abdeladim@gmail.com>
2018-01-09 22:04:04 +0000

    initial commit + headr template

---
+++ AES.vhd
@@ -0,0 +1,13 @@
+----------------------------------------------------------------------------------
+-- National Institute of Posts and Telecommunications: 
+-- student Engineer: Abdeladim SADIKI
+-- 
+-- Create Date:    21:47 09/01/2018 
+-- Module Name:    AES - Behavioral 
+-- Project Name:   AES Algorithm
+-- Description:    A VHDL implementation of AES algorithm
+
+-- Revision:       1.0
+-- Additional Comments: In collaboration with Mohamed ZELMAD
+----------------------------------------------------------------------------------
+
