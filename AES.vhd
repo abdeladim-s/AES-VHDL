ca0e9cf4f1bf5be65849aac48d7f146dbf623b7c
hp-pc <sadiki.abdeladim@gmail.com>
2018-01-09 22:38:28 +0000

    create the initial round: addRoundKey

--- AES.vhd
+++ AES.vhd
@@ -5,7 +5,7 @@
 -- Create Date:    21:47 09/01/2018 
 -- Module Name:    AES - Behavioral 
 -- Project Name:   AES Algorithm
--- Description:    A VHDL implementation of AES algorithm
+-- Description:    A VHDL implementation of The AES algorithm
 
 -- Revision:       1.0
 -- Additional Comments: In collaboration with Mohamed ZELMAD
---
+++ AddRoundKey.vhd
@@ -0,0 +1,16 @@
+-- AddRoundKey : initial round
+
+Library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity addroundkey is
+  port( Plaintext : in std_logic_vector(127 downto 0);
+        Key       : in std_logic_vector(127 downto 0);
+        addroundkey_out : out std_logic_vector(127 downto 0));
+        
+end addroundkey;
+
+architecture addroundkey_archi of addroundkey is
+begin
+  addroundkey_out <= Plaintext xor Key;
+end AES_archi;
